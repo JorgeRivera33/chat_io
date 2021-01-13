ab4cf6c3ed82642eab504ade1f28ee3923617185
JorgeRivera33 <77014837+JorgeRivera33@users.noreply.github.com>
2021-01-12 19:18:01 -0500

    Add files via upload

---
+++ client.js
@@ -0,0 +1,19 @@
+const mensajes=document.querySelector("#mensajes");
+const msgForm=document.querySelector("#msgForm");
+const socket = io('http://localhost:3000');
+
+socket.on('message',data=>{
+	console.log(data);
+	agregarMensaje(data);
+});
+
+msgForm.addEventListener('submit', e=>{
+	e.preventDefault();
+	socket.emit('chatmsg', msgForm.msg.value);
+	msgForm.msg.value="";
+});
+
+function agregarMensaje(mensaje){
+	const html=`<div>${mensaje}</div>`;
+	mensajes.innerHTML += html;
+}
